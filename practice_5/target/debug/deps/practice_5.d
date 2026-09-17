0751c845e967c0e330fb5559c50c7c18c9852c30
oobioru <oritsesemisan.obioru@pau.edu.ng>
2026-09-17 20:14:31 +0100

    Delete practice_5 directory

--- practice_5/target/debug/deps/practice_5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\HP\Desktop\COS101\week-4\practice_5\target\debug\deps\practice_5.d: src\main.rs
-
-C:\Users\HP\Desktop\COS101\week-4\practice_5\target\debug\deps\practice_5.exe: src\main.rs
-
-src\main.rs:
