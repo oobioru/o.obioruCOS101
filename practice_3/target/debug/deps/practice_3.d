ce0a8564bf4187a1eb763271b950c649aac50fa9
oobioru <oritsesemisan.obioru@pau.edu.ng>
2026-09-17 20:14:11 +0100

    Delete practice_3 directory

--- practice_3/target/debug/deps/practice_3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\HP\Desktop\COS101\week-4\practice_3\target\debug\deps\practice_3.d: src\main.rs
-
-C:\Users\HP\Desktop\COS101\week-4\practice_3\target\debug\deps\practice_3.exe: src\main.rs
-
-src\main.rs:
